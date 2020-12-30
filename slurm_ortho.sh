f59cc06e951f0d4abcdbaf01d13846b4a89c961f
husby036 <husby036@umn.edu>
2020-12-29 19:59:02 -0600

    Add prologue information to jobscripts Change to CWD in SLURM jobscripts to mirror PBS jobscripts

--- slurm_ortho.sh
+++ slurm_ortho.sh
@@ -10,6 +10,37 @@
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
+echo ________________________________________
+echo
+echo SLURM Job Log
+echo Start time: $(date)
+echo
+echo Job name: $SLURM_JOB_NAME
+echo Job ID: $SLURM_JOBID
+echo Submitted by user: $USER
+echo User effective group ID: $(id -ng)
+echo
+echo SLURM account used: $SLURM_ACCOUNT
+echo Hostname of submission: $SLURM_SUBMIT_HOST
+echo Submitted to cluster: $SLURM_CLUSTER_NAME
+echo Submitted to node: $SLURMD_NODENAME
+echo Cores on node: $SLURM_CPUS_ON_NODE
+echo Requested cores per task: $SLURM_CPUS_PER_TASK
+echo Requested cores per job: $SLURM_NTASKS
+echo Requested walltime: $SBATCH_TIMELIMIT
+echo Nodes assigned to job: $SLURM_JOB_NODELIST
+echo Running node index: $SLURM_NODEID
+echo
+echo Running on hostname: $HOSTNAME
+echo Parent PID: $PPID
+echo Process PID: $$
+echo
+echo Working directory: $SLURM_SUBMIT_DIR
+echo ________________________________________________________
+echo
+
+cd $SLURM_SUBMIT_DIR
+
 # init gdal tools
 source /opt/PGSC-2.1.0/init-gdal.sh
 
